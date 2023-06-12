65a5ead8a7a0aa69f70cc63a484d4f472286de7f
Desmond Kyeremeh <elDekyfin@gmail.com>
2023-06-12 18:47:39 +0000

    Fix lint issues

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 (() => {
   let className = 'math-tex';
   if (document.currentScript) {
-    let urlParts = document.currentScript.getAttribute('src').split('?');
+    const urlParts = document.currentScript.getAttribute('src').split('?');
     if (urlParts[1]) {
-      let queryParams = urlParts[1].split('&');
-      for (let i = 0; i < queryParams.length; i++) {
-        let param = queryParams[i].split('=');
+      const queryParams = urlParts[1].split('&');
+      for (const element of queryParams) {
+        const param = element.split('=');
         if (param[0] == 'class') {
           className = param[1];
           break;
@@ -16,7 +16,7 @@
   MathJax = {
     options: {
       processHtmlClass: className,
-      ignoreHtmlClass: '.*'
-    }
+      ignoreHtmlClass: '.*',
+    },
   };
 })();
--- plugin.js
+++ plugin.js
@@ -1,27 +1,33 @@
-tinymce.PluginManager.add('mathjax', function(editor, url) {
-
+window.tinymce.PluginManager.add('mathjax', function (editor, url) {
   // plugin configuration options
-  let settings = editor.getParam('mathjax');
-  let mathjaxClassName = settings.className || "math-tex";
-  let mathjaxTempClassName = mathjaxClassName + '-original';
-  let mathjaxSymbols = settings.symbols || {start: '\\(', end: '\\)'};
-  let mathjaxUrl = settings.lib || null;
-  let mathjaxConfigUrl = (settings.configUrl || url + '/config.js') + '?class=' + mathjaxTempClassName;
-  let mathjaxScripts = [mathjaxConfigUrl];
+  const settings = editor.getParam('mathjax');
+  const mathjaxClassName = settings.className || 'math-tex';
+  const mathjaxTempClassName = mathjaxClassName + '-original';
+  const mathjaxSymbols = settings.symbols || { start: '\\(', end: '\\)' };
+  const mathjaxUrl = settings.lib || null;
+  const mathjaxConfigUrl =
+    (settings.configUrl || url + '/config.js') +
+    '?class=' +
+    mathjaxTempClassName;
+  const mathjaxScripts = [mathjaxConfigUrl];
   if (mathjaxUrl) {
     mathjaxScripts.push(mathjaxUrl);
   }
 
   // load mathjax and its config on editor init
   editor.on('init', function () {
-    let scripts = editor.getDoc().getElementsByTagName('script');
-    for (let i = 0; i < mathjaxScripts.length; i++) {
+    const scripts = editor.getDoc().getElementsByTagName('script');
+    for (const element of mathjaxScripts) {
       // check if script have already loaded
-      let id = editor.dom.uniqueId();
-      let script = editor.dom.create('script', {id: id, type: 'text/javascript', src: mathjaxScripts[i]});
+      const id = editor.dom.uniqueId();
+      const script = editor.dom.create('script', {
+        id: id,
+        type: 'text/javascript',
+        src: element,
+      });
       let found = false;
-      for (let j = 0; j < scripts.length; j++) {
-        if (scripts[j].src == script.src) {
+      for (const element of scripts) {
+        if (element.src == script.src) {
           found = true;
           break;
         }
@@ -35,37 +41,37 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
 
   // remove extra tags on get content
   editor.on('GetContent', function (e) {
-    let div = editor.dom.create('div');
+    const div = editor.dom.create('div');
     div.innerHTML = e.content;
-    let elements = div.querySelectorAll('.' + mathjaxClassName);
-    for (let i = 0; i < elements.length; i++) {
-      let children = elements[i].querySelectorAll('span');
-      for (let j = 0; j < children.length; j++) {
-        children[j].remove();
+    const elements = div.querySelectorAll('.' + mathjaxClassName);
+    for (const element of elements) {
+      const children = element.querySelectorAll('span');
+      for (const child of children) {
+        child.remove();
       }
-      let latex = elements[i].getAttribute('data-latex');
-      elements[i].removeAttribute('contenteditable');
-      elements[i].removeAttribute('style');
-      elements[i].removeAttribute('data-latex');
-      elements[i].innerHTML = latex;
+      const latex = element.getAttribute('data-latex');
+      element.removeAttribute('contenteditable');
+      element.removeAttribute('style');
+      element.removeAttribute('data-latex');
+      element.innerHTML = latex;
     }
     e.content = div.innerHTML;
   });
 
-  let checkElement = function(element) {
+  const checkElement = function (element) {
     if (element.childNodes.length != 2) {
       element.setAttribute('contenteditable', false);
       element.style.cursor = 'pointer';
-      let latex = element.getAttribute('data-latex') || element.innerHTML;
+      const latex = element.getAttribute('data-latex') || element.innerHTML;
       element.setAttribute('data-latex', latex);
       element.innerHTML = '';
 
-      let math = editor.dom.create('span');
+      const math = editor.dom.create('span');
       math.innerHTML = latex;
       math.classList.add(mathjaxTempClassName);
       element.appendChild(math);
 
-      let dummy = editor.dom.create('span');
+      const dummy = editor.dom.create('span');
       dummy.classList.add('dummy');
       dummy.innerHTML = 'dummy';
       dummy.setAttribute('hidden', 'hidden');
@@ -75,17 +81,17 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
 
   // add dummy tag on set content
   editor.on('BeforeSetContent', function (e) {
-    let div = editor.dom.create('div');
+    const div = editor.dom.create('div');
     div.innerHTML = e.content;
-    let elements = div.querySelectorAll('.' + mathjaxClassName);
-    for (let i = 0 ; i < elements.length; i++) {
-      checkElement(elements[i]);
+    const elements = div.querySelectorAll('.' + mathjaxClassName);
+    for (const element of elements) {
+      checkElement(element);
     }
     e.content = div.innerHTML;
   });
 
   // refresh mathjax on set content
-  editor.on('SetContent', function(e) {
+  editor.on('SetContent', function (e) {
     if (editor.getDoc().defaultView.MathJax) {
       editor.getDoc().defaultView.MathJax.startup.getComponents();
       editor.getDoc().defaultView.MathJax.typeset();
@@ -93,11 +99,13 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
   });
 
   // refresh mathjax on any content change
-  editor.on('Change', function(data) {
-    elements = editor.dom.getRoot().querySelectorAll('.' + mathjaxClassName);
+  editor.on('Change', function (data) {
+    const elements = editor.dom
+      .getRoot()
+      .querySelectorAll('.' + mathjaxClassName);
     if (elements.length) {
-      for (let i = 0 ; i < elements.length; i++) {
-        checkElement(elements[i]);
+      for (const element of elements) {
+        checkElement(element);
       }
       if (editor.getDoc().defaultView.MathJax) {
         editor.getDoc().defaultView.MathJax.startup.getComponents();
@@ -110,8 +118,8 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
   editor.ui.registry.addToggleButton('mathjax', {
     text: 'Σ',
     tooltip: 'Mathjax',
-    onAction: function() {
-      let selected = editor.selection.getNode();
+    onAction: function () {
+      const selected = editor.selection.getNode();
       let target = undefined;
       if (selected.classList.contains(mathjaxClassName)) {
         target = selected;
@@ -119,28 +127,37 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
       openMathjaxEditor(target);
     },
     onSetup: function (buttonApi) {
-      return editor.selection.selectorChangedWithUnbind('.' + mathjaxClassName, buttonApi.setActive).unbind;
-    }
+      return editor.selection.selectorChangedWithUnbind(
+        '.' + mathjaxClassName,
+        buttonApi.setActive
+      ).unbind;
+    },
   });
 
   // handle click on existing
-  editor.on("click", function (e) {
-    let closest = e.target.closest('.' + mathjaxClassName);
+  editor.on('click', function (e) {
+    const closest = e.target.closest('.' + mathjaxClassName);
     if (closest) {
       openMathjaxEditor(closest);
     }
   });
 
   // open window with editor
-  let openMathjaxEditor = function(target) {
-
-    let mathjaxId = editor.id + '_' + editor.dom.uniqueId();
+  const openMathjaxEditor = function (target) {
+    const mathjaxId = editor.id + '_' + editor.dom.uniqueId();
 
     let latex = '';
     if (target) {
-      latex_attribute = target.getAttribute('data-latex');
-      if (latex_attribute.length >= (mathjaxSymbols.start + mathjaxSymbols.end).length) {
-        latex = latex_attribute.substr(mathjaxSymbols.start.length, latex_attribute.length - (mathjaxSymbols.start + mathjaxSymbols.end).length);
+      const latex_attribute = target.getAttribute('data-latex');
+      if (
+        latex_attribute.length >=
+        (mathjaxSymbols.start + mathjaxSymbols.end).length
+      ) {
+        latex = latex_attribute.substr(
+          mathjaxSymbols.start.length,
+          latex_attribute.length -
+            (mathjaxSymbols.start + mathjaxSymbols.end).length
+        );
       }
     }
 
@@ -151,27 +168,34 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
       height: 300,
       body: {
         type: 'panel',
-        items: [{
+        items: [
+          {
             type: 'textarea',
             name: 'title',
-            label: 'LaTex'
-          }, {
+            label: 'LaTex',
+          },
+          {
             type: 'htmlpanel',
-            html: '<div style="text-align:right"><a href="https://wikibooks.org/wiki/LaTeX/Mathematics" target="_blank" style="font-size:small">LaTex</a></div>'
-          }, {
+            html: '<div style="text-align:right"><a href="https://wikibooks.org/wiki/LaTeX/Mathematics" target="_blank" style="font-size:small">LaTex</a></div>',
+          },
+          {
             type: 'htmlpanel',
-            html: '<iframe id="' + mathjaxId + '" style="width: 100%; min-height: 50px;"></iframe>'
-        }]
+            html:
+              '<iframe id="' +
+              mathjaxId +
+              '" style="width: 100%; min-height: 50px;"></iframe>',
+          },
+        ],
       },
-      buttons: [{type: 'submit', text: 'OK'}],
+      buttons: [{ type: 'submit', text: 'OK' }],
       onSubmit: function onsubmit(api) {
-        let value = api.getData().title.trim();
+        const value = api.getData().title.trim();
         if (target) {
           target.innerHTML = '';
           target.setAttribute('data-latex', getMathText(value));
           checkElement(target);
         } else {
-          let newElement = editor.getDoc().createElement('span');
+          const newElement = editor.getDoc().createElement('span');
           newElement.innerHTML = getMathText(value);
           newElement.classList.add(mathjaxClassName);
           checkElement(newElement);
@@ -181,25 +205,28 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
         editor.getDoc().defaultView.MathJax.typeset();
         api.close();
       },
-      onChange: function(api) {
+      onChange: function (api) {
         var value = api.getData().title.trim();
         if (value != latex) {
           refreshDialogMathjax(value, document.getElementById(mathjaxId));
           latex = value;
         }
       },
-      initialData: {title: latex}
+      initialData: { title: latex },
     });
 
     // add scripts to iframe
-    let iframe = document.getElementById(mathjaxId);
-    let iframeWindow = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument;
-    let iframeDocument = iframeWindow.document;
-    let iframeHead = iframeDocument.getElementsByTagName('head')[0];
-    let iframeBody = iframeDocument.getElementsByTagName('body')[0];
-  
+    const iframe = document.getElementById(mathjaxId);
+    const iframeWindow =
+      iframe.contentWindow ||
+      iframe.contentDocument.document ||
+      iframe.contentDocument;
+    const iframeDocument = iframeWindow.document;
+    const iframeHead = iframeDocument.getElementsByTagName('head')[0];
+    const iframeBody = iframeDocument.getElementsByTagName('body')[0];
+
     // get latex for mathjax from simple text
-    let getMathText = function (value, symbols) {
+    const getMathText = function (value, symbols) {
       if (!symbols) {
         symbols = mathjaxSymbols;
       }
@@ -207,15 +234,15 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
     };
 
     // refresh latex in mathjax iframe
-    let refreshDialogMathjax = function(latex) {
-      let MathJax = iframeWindow.MathJax;
+    const refreshDialogMathjax = function (latex) {
+      const MathJax = iframeWindow.MathJax;
       let div = iframeBody.querySelector('div');
       if (!div) {
         div = iframeDocument.createElement('div');
         div.classList.add(mathjaxTempClassName);
         iframeBody.appendChild(div);
       }
-      div.innerHTML = getMathText(latex, {start: '$$', end: '$$'});
+      div.innerHTML = getMathText(latex, { start: '$$', end: '$$' });
       if (MathJax && MathJax.startup) {
         MathJax.startup.getComponents();
         MathJax.typeset();
@@ -224,14 +251,13 @@ tinymce.PluginManager.add('mathjax', function(editor, url) {
     refreshDialogMathjax(latex);
 
     // add scripts for dialog iframe
-    for (let i = 0; i < mathjaxScripts.length; i++) {
-      let node = iframeWindow.document.createElement('script');
-      node.src = mathjaxScripts[i];
+    for (const script of mathjaxScripts) {
+      const node = iframeWindow.document.createElement('script');
+      node.src = script;
       node.type = 'text/javascript';
       node.async = false;
       node.charset = 'utf-8';
       iframeHead.appendChild(node);
     }
-
   };
 });
